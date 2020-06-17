cd8928d3f5a1c3c2d1bbbb9375dad60c538ddaef
Gsuper36 <gsuper36@gmail.com>
2020-06-17 16:27:49 +0300

    Добавил активность с настройками

--- app/src/main/java/madsoft/com/form/Activity/SettingsActivity.java
+++ app/src/main/java/madsoft/com/form/Activity/SettingsActivity.java
@@ -1,37 +1,38 @@
 package madsoft.com.form.Activity;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.CheckBox;
 
-import madsoft.com.form.Assets;
-import madsoft.com.form.FileSystem.CacheSystem;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.preference.PreferenceFragmentCompat;
 import madsoft.com.form.R;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity {
 
-    //private CacheSystem cacheSystem;
-
-    private CheckBox checkBox;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-       // getActionBar().setDisplayHomeAsUpEnabled(true);
-      //  getActionBar().setHomeButtonEnabled(true);
-      //  getActionBar().setTitle(R.string.text_settings);
-
-      //  cacheSystem = new CacheSystem(this);
-
-      //  Log.d("CACHESYS", String.valueOf(cacheSystem.checkFile(Assets.FLAG_D)));
-
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        toolbar = findViewById(R.id.toolbar_settings);
+        toolbar.setTitle(R.string.title_activity_settings);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
-
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
     }
 }
