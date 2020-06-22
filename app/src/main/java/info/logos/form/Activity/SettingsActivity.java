07efd7c92c8d616eee2f4136cddefc48668df46b
Gsuper36 <gsuper36@gmail.com>
2020-06-22 23:01:38 +0300

    Кнопка о проекте в настройках

--- app/src/main/java/info/logos/form/Activity/SettingsActivity.java
+++ app/src/main/java/info/logos/form/Activity/SettingsActivity.java
@@ -1,16 +1,22 @@
 package info.logos.form.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.preference.PreferenceFragmentCompat;
+import info.logos.form.Assets;
 import info.logos.form.R;
+import info.logos.form.service.DownloadService;
 
 public class SettingsActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    Button btnAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,17 @@ public class SettingsActivity extends AppCompatActivity {
         toolbar.setTitle(R.string.title_activity_settings);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
+        btnAbout = findViewById(R.id.button_about);
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SlidingThemeActivity.class);
+                intent.setAction(" ");
+                 intent.putExtra(Assets.LINK, "https://sanctumlogos.info/o-proekte");
+                 intent.putExtra(Assets.TITLE, "О проекте");
+                 startActivity(intent);
+            }
+        });
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
